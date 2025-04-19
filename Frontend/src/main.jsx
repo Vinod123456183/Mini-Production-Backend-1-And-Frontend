fc59970f6c61d0c2fab212b1ca2fa0cf74bf39a2
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-04-19 12:26:52 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-// import Home from "./pages/Home";
+import Home from "./components/pages/Home.jsx";
+import Login from "./components/pages/Login.jsx";
+import SignUp from "./components/pages/SignUp.jsx";
 // import About from "./pages/About";
 // import Contact from "./pages/Contact";
 
@@ -16,9 +18,9 @@ import {
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      {/* <Route index element={<Home />} /> */}
-{/*       <Route path={"/about"} element={<About />} /> */}
-{/*       <Route path={"/contact"} element={<Contact />} /> */}
+      <Route index element={<Home />} />
+      <Route path={"/login"} element={<Login />} />
+      <Route path={"/signup"} element={<SignUp />} />
     </Route>
   )
 );
