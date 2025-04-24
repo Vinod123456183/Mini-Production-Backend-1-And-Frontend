8e618d57fab2f2cbc8960b7376639c61b8e03dac
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-04-24 14:43:08 +0530

    Update index-controller.js

--- controllers/index-controller.js
+++ controllers/index-controller.js
@@ -1,3 +1,12 @@
+const { errorHandler } = require("../utils/error");
+const bcrypt = require("bcrypt");
+const userModel = require("../models/user-model");
+const jwt = require("jsonwebtoken");
+const { formatDistanceToNow } = require("date-fns");
+const rateLimit = require("express-rate-limit");
+
+
+
 const indexController = (req, res) => {
   console.log("Index Controller");
   res.render("index");
