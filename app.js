964fdade40aee5a113f05b07ecf05d6af1487530
deeppak_1 <115033744+Vinod123456183@users.noreply.github.com>
2025-04-24 15:50:04 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
 const path = require("path");
 
@@ -6,13 +7,29 @@ require("dotenv").config();
 
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 const db = require("./config/mongoose-atlas-connection");
+// const db = require("./config/mongoose-connection");
 const indexRouter = require("./routes/index-router");
+
+// app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
 
 app.use("/", indexRouter);
 
 app.listen(process.env.PORT);
+
+
+app.use((err, req, res, next) => {
+  const status = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(status).json({
+    success: false,
+    message,
+  });
+});
+
+
